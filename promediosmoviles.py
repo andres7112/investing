64603042787dae9b706b8d023e6530049ebf5f8d
Andrés López <adclopezb@correo.udistrital.edu.co>
2021-06-24 16:36:21 -0500

    subiendo promedios móviles

--- promediosmoviles.py
+++ promediosmoviles.py
@@ -1,5 +1,20 @@
-import pandas
-datos_nasdaq = pandas.read_csv(r'nasdaq.csv')
+import numpy
+import pandas #importamos la librería para manipulación de datos
+import matplotlib.pyplot #importamos la librería para visualización de datos
+datos_nasdaq = pandas.read_csv(r'nasdaq.csv') #importamos los datos de nasdaq de un mes
 #print(datos_nasdaq)
-dataframe_nasdaq = pandas.DataFrame(datos_nasdaq)
-print(dataframe_nasdaq)
+dataframe_nasdaq = pandas.DataFrame(datos_nasdaq) #creamos un dataframe con los datos que importamos.
+#print(dataframe_nasdaq)
+matplotlib.pyplot.plot(dataframe_nasdaq.loc[:,"Date"], dataframe_nasdaq.loc[:,"Close"])
+matplotlib.pyplot.xlabel("Fecha")
+matplotlib.pyplot.ylabel("Precio Stock")
+matplotlib.pyplot.title("Gráfica de datos Nasdaq")
+matplotlib.pyplot.show()
+promedio_movil=dataframe_nasdaq.loc[:,"Close"].rolling(2).mean()
+print(promedio_movil)
+fecha=dataframe_nasdaq.loc[:,"Date"]
+matplotlib.pyplot.plot(dataframe_nasdaq.loc[:,"Date"], promedio_movil)
+matplotlib.pyplot.xlabel("Fecha")
+matplotlib.pyplot.ylabel("Precio Stock")
+matplotlib.pyplot.title("Promedios Móviles")
+matplotlib.pyplot.show()
